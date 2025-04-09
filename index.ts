357082c8f772dbd5083d5370bc9dffbf490a86e6
mohammad-k13 <mohammadkeshavarz.a.13@gmail.com>
2025-04-09 10:13:19 +0330

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { config } from "dotenv";
+
+const app = express();
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
+config({ path: envFile });
+
+app.use(cors({ origin: [process.env.CLIENT_URL as string] }));
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response, next) => {
+  res.status(200).send({hello: "world"})
+  next()
+})
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
