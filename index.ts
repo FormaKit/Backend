6bd246fb142897ed5f415cb759ec1cafe7971a1a
mohammad-k13 <mohammadkeshavarz.a.13@gmail.com>
2025-04-09 11:25:18 +0330

    add public folder and index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import path from "path";
 
 const app = express();
 const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
 config({ path: envFile });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({ origin: [process.env.CLIENT_URL as string] }));
 app.use(express.json());
 
